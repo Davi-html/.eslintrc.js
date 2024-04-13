4f462ae8e6a92e0f9e0ac565e2ada881351232f7
Davi-html <davifernandes3674@gmail.com>
2024-04-13 17:28:27 -0300

    first commit

---
+++ eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  rules: {
+    'no-console': 'off',
+  },
+
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+};
+
